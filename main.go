abd0458fc043eba8483b53a2df445ddcf6add250
ExtraHash <ExtraHash@protonmail.com>
2020-06-30 19:56:51 -0600

    0.4.1

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ import (
 var wsClients = []*websocket.Conn{}
 var channelSubs = []*ChannelSub{}
 
-const version string = "0.4.0"
+const version string = "0.4.1"
 const emptyUserID = "00000000-0000-0000-0000-000000000000"
 
 type ChannelPermission struct {
