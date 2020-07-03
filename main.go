cbc96fe2d68f01bd182c3a560376edd467da45e0
extra <extrahash@protonmail.com>
2020-07-02 23:58:34 -0600

    if started without config, copy default config

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -588,7 +589,34 @@ func writeBytesToFile(filename string, bytes []byte) bool {
 	return true
 }
 
+// Copy the src file to dst. Any existing file will be overwritten and will not
+// copy file attributes.
+func Copy(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
+}
+
 func readConfig() Config {
+	_, configErr := os.Stat("config.json")
+	if os.IsNotExist(configErr) {
+		Copy("examples/config-default.json", "config.json")
+	}
+
 	configBytes := readJSONFile("config.json")
 	var config Config
 
