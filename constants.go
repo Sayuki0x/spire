dfe1fff7357a267c4c5174670ffc9be80f8c68ee
ExtraHash <ExtraHash@protonmail.com>
2020-07-24 00:31:46 -0600

    2.5.0

--- constants.go
+++ constants.go
@@ -12,5 +12,5 @@ var channelSubs = []*ChannelSub{}
 var homedir, _ = os.UserHomeDir()
 var log *logging.Logger = logging.MustGetLogger("vex")
 
-const version string = "2.4.2"
+const version string = "2.5.0"
 const emptyUserID = "00000000-0000-0000-0000-000000000000"
